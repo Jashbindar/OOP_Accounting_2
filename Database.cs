aa659af35f13cb8c68992a71b124550204bee36c
Jashbindar <jashbindar55@gmail.com>
2024-10-28 00:48:25 +0800

    Update Database.cs

--- Database.cs
+++ Database.cs
@@ -39,16 +39,59 @@ namespace OOP_Accounting
         public async Task<List<UserAccount>> getUserDetail()
         {
             var userAccounts = new List<UserAccount>();
-            Query collectionQuery = database.Collection("User_Transaction");
-            QuerySnapshot querySnapshots = await collectionQuery.GetSnapshotAsync();
+            var collectionRef = database.Collection("User_Transaction");
+            var querySnapshot = await collectionRef.GetSnapshotAsync();
 
-            foreach (DocumentSnapshot document in querySnapshots.Documents)
+            foreach (var document in querySnapshot.Documents)
             {
+                // Get main document data
                 var data = document.ToDictionary();
-                double amount = double.Parse(data["Amount"].ToString());
+                var amount = double.Parse(data["Amount"].ToString());
                 var account = new UserAccount(amount);
+
+                // Get subcollections of this document
+                var subcollections = await document.Reference.ListCollectionsAsync().ToListAsync();
+                foreach (var subcollection in subcollections)
+                {
+                    var subDocs = await subcollection.GetSnapshotAsync();
+                    foreach (var subDoc in subDocs.Documents)
+                    {
+                        var subData = subDoc.ToDictionary();
+                        if (subData.ContainsKey("SubAmount"))
+                        {
+                            var subAmount = double.Parse(subData["SubAmount"].ToString());
+                        }
+                    }
+                }
+
                 userAccounts.Add(account);
             }
+            return userAccounts;
+        }
+
+        public async Task<List<UserAccount>> getUserDetailWithSpecificSubcollection(string documentId)
+        {
+            var userAccounts = new List<UserAccount>();
+
+            // Access specific document
+            var docRef = database.Collection("User_Transaction").Document(documentId);
+
+            // Access its specific subcollection
+            var subcollectionRef = docRef.Collection("SubTransactions");
+            var subDocs = await subcollectionRef.GetSnapshotAsync();
+
+            foreach (var subDoc in subDocs.Documents)
+            {
+                var subData = subDoc.ToDictionary();
+                // Process subcollection data
+                if (subData.ContainsKey("Amount"))
+                {
+                    var amount = double.Parse(subData["Amount"].ToString());
+                    var account = new UserAccount(amount);
+                    userAccounts.Add(account);
+                }
+            }
+        
             return userAccounts;
         }
     }
